2faac5fe73180c964fc0181d4b0cae7382f5814c
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2020-08-14 20:38:21 -0300

    Sessão 5 - exercício

---
+++ C# Completo Programação OO/Sessão 5/Exercicio01/Exercicio01/Conta.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Conta
+    {
+        //Atributos privados
+        private double _taxa = 5.0;
+
+        //Propriedades autoimplementadas
+        public int Numero { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        //Construtores
+        public Conta(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+        public Conta(int numero, string nome, double saldo) : this(numero, nome)
+        {
+            Saldo = saldo;
+        }
+
+        //Propriedades customizadas
+
+        //Outros métodos de classe
+        public void Deposito(double saldo)
+        {
+            Saldo += saldo;
+        }
+        public void Saque(double saldo)
+        {
+            Saldo -= (saldo + _taxa);
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Nome
+                + ", Saldo: $ "
+                + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ C# Completo Programação OO/Sessão 5/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variáveis locais
+            int conta;
+            string nome;
+            double valor;
+            char opcao = 'z';
+
+            //Instanciação da Classe
+            Conta conta1;
+
+            //Entrando dados
+            Console.Write("Entre o número da conta: ");
+            conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+
+            //Só aceitar se for digitado uma das opções mostradas
+            while (opcao != 's' && opcao != 'n')
+            {
+                opcao = char.Parse(Console.ReadLine());
+            }
+            
+            //Solicitando saldo inicial e chamando a classe com três variáveis
+            if (opcao == 's')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                valor = double.Parse(Console.ReadLine());
+                conta1 = new Conta(conta, nome, valor);
+            }
+            //Chamando a classe com duas variáveis
+            else
+            {
+                conta1 = new Conta(conta, nome);
+            }
+            Console.WriteLine();
+
+            //Exibindo a conta
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta1);
+            Console.WriteLine();
+
+            //Fazendo depósito
+            Console.Write("Entre um valor para depósito: ");
+            valor = double.Parse(Console.ReadLine());
+            conta1.Deposito(valor);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta1);
+            Console.WriteLine();
+
+            //Fazendo saque
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine());
+            conta1.Saque(valor);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta1);
+        }
+    }
+}
