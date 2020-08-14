853eb209fb26a24c3233aa3c2b7645b86b73504b
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2020-08-13 21:14:53 -0300

    Sessão 5 - Construtores, sobrecarga e this

---
+++ C# Completo Programação OO/Sessão 5/A51-Construtores/A42-SegundoProblema/Produto.cs
@@ -0,0 +1,51 @@
+﻿namespace A42_SegundoProblema
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()//Construtor padrão, se não tiver nenhum construtor, esse é válido mesmo se não tiver descrito
+                        //Caso tenha algum outro construtor, esse não irá funcionar se não for escrito no código
+        {
+            Quantidade = 10;
+        }
+        public Produto(string nome, double preco) : this()//Construtor com dois argumentos
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)//construtor com três argumentos
+        {
+            Quantidade = quantidade;
+        }
+        //Sobrecarga é quando você cria mais de um construtor
+
+        public double ValorTotalEmEstoque()
+        {
+            return (double) Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
---
+++ C# Completo Programação OO/Sessão 5/A51-Construtores/A42-SegundoProblema/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace A42_SegundoProblema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Produto a = new Produto(nome, preco);//Instanciar com construtor de dois argumentos
+            Produto b = new Produto();//Instanciar pelo construtor padrão
+            //Outra forma de instanciar a classe mesmo sem construtores seria:
+            //se a caso não tiver o construtor padrão, ele deixa de funcionar.
+            Produto c = new Produto()//Pode ser utilizado com ou sem parênteses
+            {
+                Nome = "TV",
+                Preco = 900.00,
+                Quantidade = 20
+            };
+
+            Console.WriteLine("Dados do produto: " + a);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int x = int.Parse(Console.ReadLine());
+            a.AdicionarProdutos(x);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + a);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            x = int.Parse(Console.ReadLine());
+            a.RemoverProduto(x);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + a);
+            Console.WriteLine();
+        }
+    }
+}
